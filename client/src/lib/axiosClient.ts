8bcd89801fc91afd04cffb5ecef1db06b13495d2
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2023-10-23 10:53:33 +0630

    fix : base api url

--- client/src/lib/axiosClient.ts
+++ client/src/lib/axiosClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = () =>
   axios.create({
-    baseURL: "http://localhost:3000/",
+    baseURL: process.env.REACT_APP_API_URL,
     timeoutErrorMessage: "The request time out at this point.",
     headers: {
       Authorization: "Bearer " + localStorage.getItem("jwt_token"),
